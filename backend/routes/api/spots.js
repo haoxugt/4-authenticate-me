cafb901ad1c993e1bef33c6c0f0225180d2ee5a7
Hao Xu <haoxugt@gmail.com>
2024-01-29 22:31:16 -0800

    Add 'Create a Spot'

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,8 +5,58 @@ const sequelize = require('sequelize');
 const { Op } = require('sequelize');
 const { User, Spot, SpotImage, Review } = require('../../db/models');
 const bcrypt = require('bcryptjs');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
 
+const validateSpotInput = [
+    check('address')
+        .notEmpty()
+        .withMessage('Street address is required.'),
+    check('city')
+        .notEmpty()
+        .withMessage('City is required.'),
+    check('state')
+        .notEmpty()
+        .withMessage('State is required.'),
+    check('country')
+        .notEmpty()
+        .withMessage('Country is required.'),
+    check('lat')
+        .isFloat({ min: -90, max: 90 })
+        .withMessage('Latitude must be within -90 and 90.')
+        .notEmpty()
+        .withMessage('Latitude is required.'),
+    check('lng')
+        .isFloat({ min: -180, max: 180 })
+        .withMessage('Longitude  must be within -180 and 180.')
+        .notEmpty()
+        .withMessage('Longitude is required.'),
+    check('name')
+        .notEmpty()
+        .isLength({ max: 50 })
+        .withMessage('Name must be less than 50 characters.'),
+    check('description')
+        .notEmpty()
+        .withMessage('Description is required.'),
+    check('price')
+        .isFloat({ min: 0.01 })
+        .withMessage('Price per day must be a positive number.')
+        .notEmpty()
+        .withMessage('Price is required.'),
+    handleValidationErrors
+];
+
+// (req, res, next) => {
+//     if (req.body.state === undefined) {
+//         const err = new Error("State is required.");
+//         err.title = "Bad request."
+//         err.status = 400;
+//         err.path = "state";
+//         throw err;
+//     } else { next();}
+// },
 async function getSpots(spots) {
     for (let spotIdx = 0; spotIdx < spots.length; spotIdx++) {
         const spot = spots[spotIdx];
@@ -19,7 +69,6 @@ async function getSpots(spots) {
         spots[spotIdx].dataValues.previewImage = previewImage;
     }
     return spots;
-
 }
 
 router.get('/', async (req, res) => {
@@ -39,8 +88,8 @@ router.get('/current', requireAuth, async (req, res, next) => {
 router.get('/:spotId', async (req, res, next) => {
     const spot = await Spot.findByPk(req.params.spotId, {
         include: [
-            {model: SpotImage, attributes: ["id", "url", "preview"]},
-            {model: User, attributes: ["id", "firstName", "lastName"]}
+            { model: SpotImage, attributes: ["id", "url", "preview"] },
+            { model: User, attributes: ["id", "firstName", "lastName"] }
         ]
     });
     if (spot) {
@@ -53,6 +102,17 @@ router.get('/:spotId', async (req, res, next) => {
         err.status = 404;
         next(err);
     }
+});
+
+router.post('/', requireAuth, validateSpotInput, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const spot = await Spot.bulkCreate([
+        {
+            ownerId: req.user.id,
+            address, city, state, country, lat, lng, name, description, price
+        }
+    ], { validate: true });
+    res.status(201).json(spot[0]);
 });
 
 router.delete('/:id', async (req, res) => {
