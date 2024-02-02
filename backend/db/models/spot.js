fb37eec1fb2da6eaa4684fde35b03028a8efb46b
Hao Xu <haoxugt@gmail.com>
2024-02-02 09:47:53 -0800

    1st clean version: remvoe float format postgresql unsupported

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -81,7 +81,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     price: {
-      type: DataTypes.FLOAT(7, 2).UNSIGNED,
+      type: DataTypes.FLOAT,
       allowNull: false
     },
   }, {
