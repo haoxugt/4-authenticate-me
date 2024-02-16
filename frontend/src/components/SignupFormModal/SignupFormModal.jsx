bbb12d8abe719adf562052177eb5e0d472e5566a
Hao Xu <haoxugt@gmail.com>
2024-02-16 15:43:12 -0800

    Set up for deployment

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 import * as sessionActions from '../../store/session';
 
 import './SignupForm.css';
 
 function SignupFormModal() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
+  // const sessionUser = useSelector(state => state.session.user);
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [firstName, setFirstName] = useState('');
