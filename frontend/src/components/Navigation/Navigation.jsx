c1a1765621b0d820f43801afa6d70646af286e14
Hao Xu <haoxugt@gmail.com>
2024-02-16 13:53:24 -0800

    Dropdown menu

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,6 +2,8 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 
+import './Navigation.css'
+
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
